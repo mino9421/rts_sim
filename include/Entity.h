67421a21e9877cec16929859022a918056267971
mino9421 <mino9421@gmail.com>
2025-04-02 01:39:10 -0400

    separated refactored entitiy and will follow up for the next few things leaning it down to SOLID

---
+++ include/Entity.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <queue>
+
+struct Entity {
+    std::string type;
+    int x, y;
+    int targetX = -1, targetY = -1;
+    bool selected = false;
+    bool isBuilding = false;
+    float buildTimer = 0.0f;
+    int buildOnX = -1, buildOnY = -1;
+    std::string buildQueued;
+    std::queue<std::string> productionQueue;
+    float productionTimer = 0.0f;
+    float harvestTimer = 0.0f;
+};
+
