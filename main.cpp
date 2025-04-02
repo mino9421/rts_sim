ead8f5c827fd3dc7c38932eff7100805c835c8dc
mino9421 <mino9421@gmail.com>
2025-04-02 18:35:39 -0400

    commented group selection because a bug that selects all units within bottom right logic

--- main.cpp
+++ main.cpp
@@ -33,21 +33,21 @@ int main() {
             GameSystem::handleKeyInput(event, state);
             GameSystem::handleMouseInput(event, state);
 
-            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-                dragStart = sf::Mouse::getPosition(window);
-                isDragging = true;
-            } else if (event.type == sf::Event::MouseMoved && isDragging) {
-                dragEnd = sf::Mouse::getPosition(window);
-            } else if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
-                isDragging = false;
-                SelectionSystem::selectUnitsInBox(
-                    sf::Vector2f(dragStart.x, dragStart.y),
-                    sf::Vector2f(dragEnd.x, dragEnd.y),
-                    state.entities
-                );
+            // if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+            //     dragStart = sf::Mouse::getPosition(window);
+            //     isDragging = true;
+            // } else if (event.type == sf::Event::MouseMoved && isDragging) {
+            //     dragEnd = sf::Mouse::getPosition(window);
+            // } else if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
+            //     isDragging = false;
+            //     SelectionSystem::selectUnitsInBox(
+            //         sf::Vector2f(dragStart.x, dragStart.y),
+            //         sf::Vector2f(dragEnd.x, dragEnd.y),
+            //         state.entities
+            //     );
                 
-                dragStart = dragEnd = {-1, -1};
-            }
+            //     dragStart = dragEnd = {-1, -1};
+            // }
         }
 
         GameSystem::updateEntities(dt, state);
@@ -58,12 +58,12 @@ int main() {
         RenderSystem::drawEntities(window, state.entities);
         RenderSystem::drawBuildings(window, state.buildings);
 
-        if (isDragging && dragStart.x >= 0 && dragEnd.x >= 0) {
-            SelectionSystem::drawSelectionBox(window, isDragging,
-                sf::Vector2f(dragStart.x, dragStart.y),
-                sf::Vector2f(dragEnd.x, dragEnd.y)
-            );
-        }
+        // if (isDragging && dragStart.x >= 0 && dragEnd.x >= 0) {
+        //     SelectionSystem::drawSelectionBox(window, isDragging,
+        //         sf::Vector2f(dragStart.x, dragStart.y),
+        //         sf::Vector2f(dragEnd.x, dragEnd.y)
+        //     );
+        // }
 
         RenderSystem::drawHUD(window, state);
         window.display();
